2deef10509077e55945da8b87c4b1a1e2cc4db46
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2025-08-13 13:22:10 +0400

    Minor updates

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import 'devextreme/dist/css/dx.fluent.blue.dark.css';
 import './App.css';
 import { TileView } from 'devextreme-react/tile-view';
@@ -6,7 +5,7 @@ import { tiles } from './data.js';
 
 function TileRender(data: { icon: string; title: string; text: string }): JSX.Element {
   return (
-    <React.Fragment>
+    <>
       <div className="icon-box">
         <i className={`dx-icon-${data.icon}`}></i>
       </div>
@@ -14,26 +13,25 @@ function TileRender(data: { icon: string; title: string; text: string }): JSX.El
         <h3 className="tile-title">{data.title}</h3>
         <p className="tile-text">{data.text}</p>
       </div>
-    </React.Fragment>
+    </>
   );
 }
 
 function App(): JSX.Element {
   return (
-    <React.Fragment>
+    <>
       <div className='demo-container'>
         <TileView
           dataSource={tiles}
           itemMargin={16}
           baseItemWidth={280}
           baseItemHeight={160}
-          width={608}
           height={368}
           activeStateEnabled={false}
           itemRender={TileRender}
         />
       </div>
-    </React.Fragment>
+    </>
   );
 }
 
