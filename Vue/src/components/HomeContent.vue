ea24eea91febc40849156e4e7f850f2359b2cc3e
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2025-08-13 10:00:33 +0400

    Fix linting

--- Vue/src/components/HomeContent.vue
+++ Vue/src/components/HomeContent.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import 'devextreme/dist/css/dx.fluent.blue.dark.css';
-import { DxTileView, type DxTileViewTypes } from 'devextreme-vue/tile-view';
-import notify from 'devextreme/ui/notify';
-import { formatNumber } from 'devextreme/localization';
+import { DxTileView } from 'devextreme-vue/tile-view';
 import { tiles } from '@/data.js';
 
 </script>
@@ -23,8 +21,8 @@ import { tiles } from '@/data.js';
           <i :class="`dx-icon-${data.data.icon}`"></i>
         </div>
         <div class="text-box">
-          <h3 class="tile-title">{{data.data.title}}</h3>
-          <p class="tile-text">{{data.data.text}}</p>
+          <h3 class="tile-title">{{ data.data.title }}</h3>
+          <p class="tile-text">{{ data.data.text }}</p>
         </div>
       </template>
     </DxTileView>
