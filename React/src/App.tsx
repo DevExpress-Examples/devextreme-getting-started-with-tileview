dcd230acf9937ff5de1d93df010173dec92f9066
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2025-08-13 13:33:52 +0400

    Rework React (#8)

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,45 +1,37 @@
-import React from 'react';
+import 'devextreme/dist/css/dx.fluent.blue.dark.css';
 import './App.css';
-import 'devextreme/dist/css/dx.material.blue.light.compact.css';
-import { TileView, type TileViewTypes } from 'devextreme-react/tile-view';
-import notify from 'devextreme/ui/notify';
-import { formatNumber } from 'devextreme/localization';
-import { bikes } from './data.js';
+import { TileView } from 'devextreme-react/tile-view';
+import { tiles } from './data.js';
 
-function TileRender(itemData: { price: number; disabled: boolean | undefined; imageSrc: string }): JSX.Element {
-  if (!itemData.disabled) {
-    return (
-      <div className="image" style={{ backgroundImage: `url(${itemData.imageSrc})` }}>
-        <p className="price-text">{ formatNumber(itemData.price, 'currency') }</p>
-      </div>
-    );
-  }
-
+function TileRender(data: { icon: string; title: string; text: string }): JSX.Element {
   return (
-    <div className="image" style={{ backgroundImage: `url(${itemData.imageSrc})`, opacity: '50%' }}>
-      <p className="price-text">Out of stock</p>
-    </div>
+    <>
+      <div className="icon-box">
+        <i className={`dx-icon-${data.icon}`}></i>
+      </div>
+      <div className="text-box">
+        <h3 className="tile-title">{data.title}</h3>
+        <p className="tile-text">{data.text}</p>
+      </div>
+    </>
   );
-}
-
-function HandleTileClick(e: TileViewTypes.ItemClickEvent): void {
-  notify(`Price: ${formatNumber(e.itemData.price, 'currency')}`);
 }
 
 function App(): JSX.Element {
   return (
-    <React.Fragment>
-      <TileView
-        dataSource={bikes}
-        itemMargin={5}
-        baseItemWidth={175}
-        baseItemHeight={175}
-        showScrollbar="always"
-        height={370}
-        itemRender={TileRender}
-        onItemClick={HandleTileClick}
-      />
-    </React.Fragment>
+    <>
+      <div className='demo-container'>
+        <TileView
+          dataSource={tiles}
+          itemMargin={16}
+          baseItemWidth={280}
+          baseItemHeight={160}
+          height={368}
+          activeStateEnabled={false}
+          itemRender={TileRender}
+        />
+      </div>
+    </>
   );
 }
 
